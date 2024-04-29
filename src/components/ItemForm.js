cb78f681a073486e3c8f1d2540c70e28a87aea20
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-04-29 14:17:46 +0300

    complete lab

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -4,8 +4,27 @@ function ItemForm() {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+      .then((r) => r.json())
+      .then((newItem) => console.log(newItem));
+  }
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
@@ -29,7 +48,7 @@ function ItemForm() {
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" onSubmit={handleSubmit}>Add to List</button>
     </form>
   );
 }
